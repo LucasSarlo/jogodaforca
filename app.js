f2de6d30aefc87cf69a6ba8f75066fadfe4c82ce
Lucas Sarlo <lucas.dsantos@al.infnet.edu.br>
2017-10-27 19:52:04 +0000

    comecando a forca

---
+++ app.js
@@ -0,0 +1,17 @@
+var forca = function(){
+    var erros = '';
+    var acertos = '';
+    var palavras = ['mochila', 'bananada', 'papibaquigrafo'];
+    var inicia = function(){
+    };
+    var verificaLetra = function(){
+    };
+    return{
+        arros : erros,
+        acertos: acertos,
+        inicia: inicia,
+        verificaLetra: verificaLetra
+    };
+}
+var partida = new forca();
+partida.inicia();
